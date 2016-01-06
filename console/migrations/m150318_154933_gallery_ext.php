576552e8aca048fa4333ada1d2553cd07645bdd4
serg <serg@berserker-ws>
2016-01-06 21:48:40 +0200

    06.01.2016 - добавлены следущие компоненты: - zxbodya\yii2-gallery-manager

---
+++ console/migrations/m150318_154933_gallery_ext.php
@@ -0,0 +1,14 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * Class m150318_154933_gallery_ext
+ *
+ * @package 
+ */
+class m150318_154933_gallery_ext extends zxbodya\yii2\galleryManager\migrations\m140930_003227_gallery_manager 
+{
+    //put your code here
+}
